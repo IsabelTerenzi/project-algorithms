c5e7abda0129f6dcb2b4dd26f234b37188d11c28
Isabel Terenzi <bel.terenzi@gmail.com>
2023-02-16 16:03:49 -0300

    requisito 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,17 @@
+# Requisito 3: Palíndromos (Recursividade)
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+   # Se for passado uma string vazia, retorne False
+   if word == "":
+      return False
+   # Retorne False se a palavra passada por parâmetro
+   # não for um palíndromo
+   if word[low_index] != word[high_index]:
+      return False
+   # Retorne True se a palavra passada por parâmetro
+   # for um palíndromo
+   if low_index >= high_index:
+      return True
+   # Chama a função novamente, agora analisando o índice seguinte
+   # ao menor e o anterior ao maior, e assim sucessivamente
+   return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    
