bbf7bf0fe3355deb2b1812609035882442853727
Isabel Terenzi <bel.terenzi@gmail.com>
2023-02-16 15:00:48 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
+# Requisito 1: Número de estudantes estudando no mesmo horário (Algoritmo de busca)
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    numero_estudantes = 0
+
+    try:
+        for inicio, fim in permanence_period:
+            if inicio <= target_time <= fim:
+                numero_estudantes += 1
+    except TypeError:
+        return None
+    else:
+        return numero_estudantes
